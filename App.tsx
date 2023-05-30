20e98ac01182235979223fc924f40ca6f9b6654e
Javel Wilson <javelawilson@gmail.com>
2023-05-30 07:53:56 -0500

    Remove unused imports

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 
 export default function App() {
